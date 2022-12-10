dd9cbea56d213a9e5e984275a1e5d18dd79104c1
f4nn-x <francis.beatriz.bb@gmail.com>
2022-12-10 14:45:19 -0300

    little changes into the dates

--- main.js
+++ main.js
@@ -53,6 +53,8 @@ document.querySelector("#cards").innerHTML =
       creatCard(
         "24/11",
         "quinta",
+        creatGames("swiss", "7:00", "camarões") +
+        creatGames("uruguay", "10:00", "southkorea") +
         creatGames("portugal", "13:00", "ghana") +
         creatGames("brazil", "16:00", "serbia")
       )
@@ -60,13 +62,119 @@ document.querySelector("#cards").innerHTML =
       creatCard(
         "25/11",
         "sexta",
+        creatGames("wales", "7:00", "iran") +
+        creatGames("qatar", "10:00", "senegal") +
         creatGames("netherlands", "13:00", "ecuador") +
         creatGames("england", "16:00", "usa")
       )
       +
       creatCard(
+        "26/11",
+        "sábado",
+        creatGames("australia", "7:00", "tunisia") +
+        creatGames("poland", "10:00", "arabia") +
+        creatGames("france", "13:00", "denmark") +
+        creatGames("argentina", "16:00", "mexico")
+      )
+      +
+      creatCard(
+        "27/11",
+        "domingo",
+        creatGames("japan", "7:00", "costarica") +
+        creatGames("belgium", "10:00", "marrocos") +
+        creatGames("croatica", "13:00", "canada") +
+        creatGames("spain", "16:00", "german")
+      )
+      +
+      creatCard(
         "28/11",
         "segunda",
+        creatGames("camarões", "7:00", "serbia") +
+        creatGames("southkorea", "10:00", "ghana") +
         creatGames("brazil", "13:00", "swiss") +
         creatGames("portugal", "16:00", "uruguay")
+      )
+      +
+      creatCard(
+        "29/11",
+        "terça",
+        creatGames("ecuador", "12:00", "senegal") +
+        creatGames("netherlands", "12:00", "qatar") +
+        creatGames("iran", "16:00", "usa") +
+        creatGames("wales", "16:00", "england")
+      )
+      +
+      creatCard(
+        "30/11",
+        "quarta",
+        creatGames("tunisia", "12:00", "france") +
+        creatGames("australia", "12:00", "denmark") +
+        creatGames("poland", "16:00", "argentina") +
+        creatGames("arabia", "16:00", "mexico")
+      )
+      +
+      creatCard(
+        "01/12",
+        "quinta",
+        creatGames("croatia", "12:00", "belgium") +
+        creatGames("canada", "12:00", "marrocos") +
+        creatGames("japan", "16:00", "spain") +
+        creatGames("costarica", "16:00", "german")
+      )
+      +
+      creatCard(
+        "02/12",
+        "sexta",
+        creatGames("southkorea", "12:00", "portugal") +
+        creatGames("ghana", "12:00", "uruguay") +
+        creatGames("serbia", "16:00", "swiss") +
+        creatGames("brazil", "16:00", "camarões")
+      )
+      +
+      creatCard(
+        "03/12",
+        "sábado",
+        creatGames("netherlands", "12:00", "usa") +
+        creatGames("argentina", "16:00", "australia")
+      )
+      +
+      creatCard(
+        "04/12",
+        "domingo",
+        creatGames("france", "12:00", "poland") +
+        creatGames("england", "16:00", "senegal")
+      )
+      +
+      creatCard(
+        "05/12",
+        "segunda",
+        creatGames("japan", "12:00", "croatia") +
+        creatGames("brazil", "16:00", "southkorea")
+      )
+      +
+      creatCard(
+        "06/12",
+        "terça",
+        creatGames("marrocos", "12:00", "spain") +
+        creatGames("portugal", "16:00", "swiss")
+      )
+      +
+      creatCard(
+        "09/12",
+        "sexta",
+        creatGames("croatia", "12:00", "brazil") +
+        creatGames("netherlands", "16:00", "argentina")
+      )
+      +
+      creatCard(
+        "10/12",
+        "sábado",
+        creatGames("marrocos", "12:00", "portugal") +
+        creatGames("england", "16:00", "france")
+      )
+      +
+      creatCard(
+        "13/12",
+        "terça",
+        creatGames("argentina", "16:00", "croatia")
       )
